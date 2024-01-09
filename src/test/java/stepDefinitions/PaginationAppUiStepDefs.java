0965a08b5384f1f13a34e8d0c7208567b762c2a5
Prash-Java <93462306+Prash-Java@users.noreply.github.com>
2024-01-09 13:44:46 +0530

    Update PaginationAppUiStepDefs.java
    Added step defs for API tests having Rest assured, imported from main branch

--- src/test/java/stepDefinitions/PaginationAppUiStepDefs.java
+++ src/test/java/stepDefinitions/PaginationAppUiStepDefs.java
@@ -55,6 +55,47 @@ public class PaginationAppUiStepDefs {
     public void userValidatedTheTableTitleForEmailForPaginationPage(String email) {
         paginationAppUiPage.assertEmailDetailColumnValue(email);
     }
+
+    @Given("^User will assert params after hitting base uri for pagination app \"([^\"]*)\"$")
+    public void userWillAssertParamsAfterHittingBaseUriForPaginationApp(String baseUrl) {
+        RestAssured.baseURI = baseUrl;
+        request = RestAssured.given();
+        response = request.get("/users?name=&page=0&size=10");
+
+        jsonString = response.asString();
+
+        // Assert on Status Code
+        theStatusCode = JsonPath.from(jsonString).get("statusCode");
+        Assert.assertTrue(theStatusCode == 200);
+
+        // Assert On Status Message
+        theStatus = JsonPath.from(jsonString).get("status");
+        Assert.assertEquals(theStatus, "200 OK");
+
+        // Assert On Total Elements On Http.GET request
+        theTotalUsersCount = JsonPath.from(jsonString).get("data.page.totalElements");
+        Assert.assertEquals(theTotalUsersCount, 100);
+
+        // Assert On Total Pages On Http.GET request
+        theTotalPages = JsonPath.from(jsonString).get("data.page.totalPages");
+        Assert.assertEquals(theTotalPages, 10);
+
+        // Assert On Size On Http.GET request
+        theSize = JsonPath.from(jsonString).get("data.page.size");
+        Assert.assertEquals(theSize, 10);
+
+        // Assert On Number Of Elements On Http.GET request
+        theNumberOfElements = JsonPath.from(jsonString).get("data.page.numberOfElements");
+        Assert.assertEquals(theNumberOfElements, 10);
+
+        // Assert On Any UserName On Http.GET request
+        String userName = JsonPath.from(jsonString).get("data.page.content[0].name");
+        Assert.assertTrue(userName.contains("Royce".trim()));
+
+        // Assert On Any Email On Http.GET request
+        String userEmail = JsonPath.from(jsonString).get("data.page.content[5].email");
+        Assert.assertTrue(userEmail.contains("mbaildon5@census.gov".trim()));
+    }
 }
 
 
