04b64080c39489e332d74f9357a4d150de7f3a30
Prash-Java <93462306+Prash-Java@users.noreply.github.com>
2024-01-09 13:50:42 +0530

    Update PaginationAppUiStepDefs.java
    Added imports for Rest Assured API Tests

--- src/test/java/stepDefinitions/PaginationAppUiStepDefs.java
+++ src/test/java/stepDefinitions/PaginationAppUiStepDefs.java
@@ -4,6 +4,11 @@ import com.google.inject.Inject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import pages.PaginationAppUiPage;
 
 public class PaginationAppUiStepDefs {
